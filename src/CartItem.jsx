a3af6e85d48e72b0f77ee7bc4ee511134ec7e505
Nobi <nobimj@gmail.com>
2026-05-03 10:07:10 -0400

    Task 3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,11 +9,17 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+    cart.forEach((item) => {
+        total += parseFloat(item.cost.substring(1)) * item.quantity;
+    });
+
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e)
   };
 
 
@@ -37,7 +43,9 @@ const CartItem = ({ onContinueShopping }) => {
   const handleRemove = (item) => {
     dispatch(removeItem(item.name));
   };
-
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
     return parseFloat(item.cost.substring(1)) * item.quantity;
